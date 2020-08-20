25d89fa04568f8587789f84390c82783cc96b247
cstevenson7 <cindy.stevenson1@gmail.com>
2020-08-20 09:22:38 -0600

    commit

--- js/main.js
+++ js/main.js
@@ -6,35 +6,35 @@ function getJson()
     .then(response => response.json())
     .then(rawData => {        
 
-        for(let i = 0; i < 7; i++){
-            console.log(i)
-            let position = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].position
-            let position_text = document.querySelector('#position')
-            position_text.innerHTML = position
+        // for(let i = 0; i < 7; i++){
+        //     console.log(i)
+        //     let position = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].position
+        //     let position_text = document.querySelector('#position')
+        //     position_text.innerHTML = position
             
-            // let givenName = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.givenName
-            // let givenName_text = document.querySelector('#givenName')
-            // givenName_text.innerHTML = givenName
-            // console.log(givenName_text)
+        //     // let givenName = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.givenName
+        //     // let givenName_text = document.querySelector('#givenName')
+        //     // givenName_text.innerHTML = givenName
+        //     // console.log(givenName_text)
 
-            let familyName = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.familyName
-            let familyName_text = document.querySelector('#familyName')
-            familyName_text.innerHTML = familyName
-            console.log(familyName_text)
+        //     let familyName = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.familyName
+        //     let familyName_text = document.querySelector('#familyName')
+        //     familyName_text.innerHTML = familyName
+        //     console.log(familyName_text)
 
-            let nationality = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.nationality
-            let nationality_text = document.querySelector('#nationality')
-            nationality_text.innerHTML = nationality
+        //     let nationality = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.nationality
+        //     let nationality_text = document.querySelector('#nationality')
+        //     nationality_text.innerHTML = nationality
 
-            let sponsor = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Constructors[0].name;
-            let sponsor_text = document.querySelector('#sponsor')
-            sponsor_text.innerHTML = sponsor
+        //     let sponsor = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Constructors[0].name;
+        //     let sponsor_text = document.querySelector('#sponsor')
+        //     sponsor_text.innerHTML = sponsor
 
-            let points = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].points
-            let points_text = document.querySelector('#points')
-            points_text.innerHTML = points
+        //     let points = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].points
+        //     let points_text = document.querySelector('#points')
+        //     points_text.innerHTML = points
                       
-        }
+        // }
 
         // //use a for loop to the /top 7 F1 drivers
         
@@ -51,7 +51,7 @@ function getJson()
                 let newText1 = document.createTextNode(tbl_position);
                 newCell1.appendChild(newText1);
 
-                // All other cells are added to the bottom fot the table (-1) - NAME
+                // All other cells are added to the bottom of the table (-1) - NAME
                 let newCell2 = newRow.insertCell(-1);
                 // Append a text node to the cell
                 let tbl_givenName = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.givenName
@@ -59,24 +59,22 @@ function getJson()
                 let fullName =  tbl_givenName + " " + tbl_lastname           
                 let newText2 = document.createTextNode( fullName);
                 newCell2.appendChild(newText2);
-
-
-
-                // All other cells are added to the bottom fot the table (-1) - NATIONALITY
+                
+                // All other cells are added to the bottom of the table (-1) - NATIONALITY
                 let newCell3 = newRow.insertCell(-1);
                 // Append a text node to the cell
                 let tbl_nationality = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Driver.nationality    
                 let newText3 = document.createTextNode(tbl_nationality);
                 newCell3.appendChild(newText3);
 
-                // All other cells are added to the bottom fot the table (-1) - SPONSOR
+                // All other cells are added to the bottom of the table (-1) - SPONSOR
                 let newCell4 = newRow.insertCell(-1);
                 // Append a text node to the cell
                 let constructors_name = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Constructors[0].name;          
                 let newText4 = document.createTextNode( constructors_name );
                 newCell4.appendChild(newText4);
 
-                // All other cells are added to the bottom fot the table (-1) - POINTS
+                // All other cells are added to the bottom of the table (-1) - POINTS
                 let newCell5 = newRow.insertCell(-1);
                 // Append a text node to the cell
                 let tbl_points = rawData.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].points        
